d12192adacb3cff0775d32450ccb0c9d9040036a
mpazba9 <36500504+mpazba9@users.noreply.github.com>
2018-02-26 12:03:36 +0100

    Add files via upload

--- window.py
+++ window.py
@@ -1,5 +1,5 @@
 
-seq1 = "AMGDEF"
+seq1 = "AMGDEFAM"
 
 dictioseq = {}
 dictioseq["A"] = [1,0,0,0,0,0]
@@ -10,27 +10,18 @@ dictioseq["F"] = [0,0,0,0,1,0]
 dictioseq["M"] = [0,0,0,0,0,1]
 
 
-for i in range(0,len(seq1)): 
-    b = [seq1[i:i+3] for i in range(len(seq1)-2)]
-    #print(seq1[i])
-    
-print(b) 
-print(dictioseq)
-
-for aa in b: 
-    
-
-   
      
+window_size = 3
 
-#Both codes work    
-"""def window(seq, window_size=5):
-    for i in range(len(seq) - window_size + 1):
-        yield seq[i:i+window_size]
-for seq in window(seq1, 3):
-    print(seq)
-    #print(dictioseq)
 
-merged = dictioseq.values(seq)
-print(merged)"""
+def window(seq, window_size):
+    for i in range(len(seq)- window_size):
+        return(seq[i:i+window_size])
 
+for i in range(len(seq1)-window_size):
+    print(seq1[i:i+window_size])
+    for i in seq1[i:i+window_size]:
+        print(dictioseq[i])    
+  
+    
+    
